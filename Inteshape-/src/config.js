2b945096efe10ad11667ef12191ea92cf3078f22
Pradeep Pal <pradeeppal8867256@gmail.com>
2026-07-29 16:08:35 +0530

    .env file add

--- Inteshape-/src/config.js
+++ Inteshape-/src/config.js
@@ -1,4 +1,13 @@
+const isProduction = process.env.NODE_ENV === "production";
+
+const rawApiUrl = isProduction
+  ? process.env.REACT_APP_API_URL_PROD
+  : process.env.REACT_APP_API_URL_DEV;
+
+const apiUrl = (rawApiUrl || "http://localhost:5000").replace(/\/+$/, "");
+
 const config = {
-  apiUrl: "http://localhost:5000",  
+  apiUrl,
 };
+
 export default config;
